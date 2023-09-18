e86a9960fee55ba882d4d98061bdf73f574ff95b
Gerardo Burucua <47932497+gburucua@users.noreply.github.com>
2023-09-19 00:27:44 +0200

    Update build_and_push.sh

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 # Set AWS instance IP address and other variables
-AWS_INSTANCE_IP="34.230.161.156"
+#AWS_INSTANCE_IP="34.230.161.156"
 DOCKER_IMAGE_NAME="pythonapp"
 DOCKER_IMAGE_TAG="latest"
-REPOSITORY_NAME="gburucua"
+REPOSITORY_NAME="pythonapp"
 
 
 # Set the SSH private key from the GitHub secret
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
+#Docker login
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
 # Build the Docker image
-docker build -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .
+#docker build -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .
+docker build -t $DOCKER_USERNAME/$REPOSITORY_NAME:$IMAGE_TAG .
+
 
 # Push the Docker image to a registry (e.g., Docker Hub)
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push $DOCKER_USERNAME/$REPOSITORY_NAME:$IMAGE_TAG
+
 
 # SSH into your AWS instance and deploy the updated image
 #ssh -i ~/.ssh/id_rsa ec2-user@$AWS_INSTANCE_IP <<EOF
