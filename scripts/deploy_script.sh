dd5aeec5b7062f25cf585b3f4b8822c1e176117e
Gerardo Burucua <47932497+gburucua@users.noreply.github.com>
2023-09-18 22:08:41 +0200

    Update deploy_script.sh

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-# Set your AWS instance IP address and SSH key file
+# Set AWS instance IP address and other variables
 AWS_INSTANCE_IP="34.230.161.156"
-SSH_KEY_FILE="C:\Users\Buruc\OneDrive\Escritorio\Projecto personal\gbkey.pem"
+DOCKER_IMAGE_NAME="pythonapp"
+DOCKER_IMAGE_TAG="latest"
 
-# Directory to sync (update this based on your project)
-SOURCE_DIR="C:\Users\Buruc\OneDrive\Escritorio\Projecto personal\pythonapp\"
+# Set the SSH private key from the GitHub secret
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 
-# SSH into the AWS instance and sync files
-ssh -i "$SSH_KEY_FILE" ec2-user@"$AWS_INSTANCE_IP" "rsync -avz --delete $SOURCE_DIR /home/ec2-user/pythonapp"
+# Build the Docker image
+docker build -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .
+
+# Push the Docker image to a registry (e.g., Docker Hub)
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker push $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+
+# SSH into your AWS instance and deploy the updated image
+ssh -i ~/.ssh/id_rsa ec2-user@$AWS_INSTANCE_IP <<EOF
+  docker pull $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+  docker stop pythonapp
+  docker rm pythonapp
+  docker run -d -p 80:80 --name pythonapp $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+EOF
