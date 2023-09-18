1ff8ba2da90a89997fdad3191316538149286ed0
Gerardo Burucua <47932497+gburucua@users.noreply.github.com>
2023-09-18 23:47:06 +0200

    Create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set AWS instance IP address and other variables
+AWS_INSTANCE_IP="34.230.161.156"
+DOCKER_IMAGE_NAME="pythonapp"
+DOCKER_IMAGE_TAG="latest"
+
+# SSH into your AWS instance and deploy the updated image
+ssh -i ~/.ssh/id_rsa ec2-user@$AWS_INSTANCE_IP <<EOF
+  docker pull $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+  docker stop pythonapp
+  docker rm pythonapp
+  docker run -d -p 80:80 --name pythonapp $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+EOF
