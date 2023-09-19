d357ec0208a0483c8cffb437556e270015971592
Gerardo Burucua <47932497+gburucua@users.noreply.github.com>
2023-09-19 11:01:55 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,9 @@ AWS_INSTANCE_IP="34.230.161.156"
 DOCKER_IMAGE_NAME="pythonapp"
 DOCKER_IMAGE_TAG="latest"
 
+# Authenticate with Docker Hub
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+
 # SSH into your AWS instance and deploy the updated image
 ssh -i ~/.ssh/id_rsa ec2-user@$AWS_INSTANCE_IP <<EOF
   docker pull $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
